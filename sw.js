cc6675c3804e60f454dce9ef2a83dc8c0f5ed6fb
zaaffeerrahamed-cell <zaaffeerrahamed@gmail.com>
2026-03-18 16:47:35 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'champ-v1';
+const ASSETS = ['/champ/', '/champ/index.html', '/champ/manifest.json', '/champ/icon-192.png', '/champ/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/champ/index.html')))
+  );
+});
